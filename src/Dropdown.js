b46fc496c088e15ab12de155929d16a43fd7fc68
Daniel Johansson <daniel@codebymini.se>
2022-10-27 12:48:22 +0200

    init

---
+++ src/Dropdown.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "./Dropdown.css";
+import { useTranslation } from "react-i18next";
+
+export default function Dropdown() {
+  const { t, i18n } = useTranslation();
+  const [currentLang, setLanguage] = useState(i18n.language);
+
+  const changeLanguageHandler = (lang) => {
+    setLanguage(lang);
+    i18n.changeLanguage(lang);
+  };
+  return (
+    <div className="dropdown">
+      <button className="dropbtn">{t("languages." + currentLang)}</button>
+      <div className="dropdown-content">
+        <div
+          className="language"
+          onClick={() => {
+            changeLanguageHandler("sv");
+          }}
+        >
+          {t("languages.sv")}
+        </div>
+        <div
+          className="language"
+          onClick={() => {
+            changeLanguageHandler("en");
+          }}
+        >
+          {t("languages.en")}
+        </div>
+      </div>
+    </div>
+  );
+}
